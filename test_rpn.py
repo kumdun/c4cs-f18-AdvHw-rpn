8e27c9e083b92ef44974afdff3f8d9873bad5e5f
Duncan Kummer <kumdun@umich.edu>
2018-10-26 15:16:51 -0400

    Test for too many numbers

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,9 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
+    def test_sub(self):
+        result = rpn.calculate("1 1 -")
+        self.assertEqual(0, result)
+    def test_too_many(self):
+        with self.assertRaises(ValueError):
+            result = rpn.calculate('1 2 3 +')
